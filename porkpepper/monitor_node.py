6b9ebaa4953c14cba64904d44589c410c52bf3d6
宋伟(songwei) <songwei@songwei.io>
2019-06-23 02:30:44 +0800

    MOD MonitorNode

--- porkpepper/monitor_node.py
+++ porkpepper/monitor_node.py
@@ -13,42 +13,47 @@ class Session(WebsocketSession):
 
 class SimpleMonitorNode(WebsocketNode):
     TIMER_TASK = None
-    TIMER_INTERVAL = 2
+    TIMER_INTERVAL = 5
     NODE_URL_LIST = list()
 
     def __init__(self, websocket_path="/monitor"):
         super(SimpleMonitorNode, self).__init__(session_class=WebsocketSession, websocket_path=websocket_path)
 
+    @classmethod
+    async def command_collect(cls, node_url, report_list):
+        connectivity = False
+        conn = None
+        info = None
+        try:
+            conn = await aioredis.create_redis(node_url, timeout=1)
+            info = await conn.info()
+            connectivity = True
+        except OSError as e:
+            connectivity = False
+        finally:
+            if conn:
+                conn.close()
+            report_list.append(
+                {
+                    "nodeUrl": node_url,
+                    "connectivity": connectivity,
+                    "info": info,
+                }
+            )
+
     async def publish_nodes_status(self, interval=0):
         report_list = list()
-        for node_url in self.NODE_URL_LIST:
-            connectivity = False
-            conn = None
-            info = None
-            try:
-                conn = await aioredis.create_redis(node_url)
-                info = await conn.info()
-                connectivity = True
-            except OSError as e:
-                connectivity = False
-            finally:
-                if conn:
-                    conn.close()
-                report_list.append(
-                    {
-                        "nodeUrl": node_url,
-                        "connectivity": connectivity,
-                        "info": info,
-                    }
-                )
+        if self.NODE_URL_LIST:
+            await asyncio.gather(*[self.command_collect(node_url, report_list) for node_url in self.NODE_URL_LIST])
+        report_list = sorted(report_list, key=lambda x: x["nodeUrl"])
         message = dict(type="report", detail=report_list)
         app = self._http_app
         if app is not None:
             all_sessions = self._http_app.all_sessions()
             send_list = [session.send(message) for session in all_sessions]
             if send_list:
-                task = asyncio.Task(asyncio.gather(*send_list))
-                asyncio.ensure_future(task)
+                asyncio.ensure_future(asyncio.gather(*send_list))
+
 
     async def on_start(self):
         if self.TIMER_TASK:
