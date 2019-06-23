b5f916e466f40594cbb35a55e6b466d0d4993d53
宋伟(songwei) <songwei@songwei.io>
2019-06-23 12:22:28 +0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='porkpepper',
-    version='0.1.0',
+    version='0.2.0',
     author='宋伟(songwei)',
     author_email='songwei@songwei.io',
     long_description='',
